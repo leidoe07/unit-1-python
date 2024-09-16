fed9ee59befcf22d2a54b561c867fae93d9b130a
Leilani Dore <ldore@brooklynsteamcenter.org>
2024-09-16 10:48:30 -0400

    lesson

---
+++ lesson_00.py
@@ -0,0 +1,41 @@
+# 1. Create a new file using snake case format, called "lesson_00.py"
+# 2. in this file print your name to the console
+
+print("Leilani Dore")
+#3. usingsnake  case format create a variable named "fav show" that store
+
+fav_show = "Interveiw with the Vampire"
+
+# 4. using the snake casing format create one float and one integer variable
+
+one_float = 2.5
+
+one_int = 3
+
+# 5.print the product of those two previous variables
+print(one_float * one_int)
+
+# 6.create A variable that stores your favorite foods
+
+fav_foods = ["sushi","pasta","pizza"]
+# 7. create an if statement that tells someone if they can get a liscense
+ur_age=18
+
+if ur_age==18:
+    print ("You can get a liscense")
+
+# 8. ask a  user for their name and print it to the console
+user_name =input("what is ur name")
+print("hello"+ user_name + "   " )
+
+#9. create a loop that prints each of your favorite foods
+
+
+for  x in fav_foods:
+    print(x)
+
+# 10. create a while loop that wil never run
+
+while x in fav_foods:
+     
+    break 
