a9e0f7f615696d73429d1999e2c953125c46465e
Leilani Dore <ldore@brooklynsteamcenter.org>
2024-09-30 10:52:11 -0400

    finid

--- projects/project01_todo_tracker.py
+++ projects/project01_todo_tracker.py
@@ -1,18 +1,30 @@
+
+with open("task_lists.txt") as file:
+    user_tasks = file.readlines()
 user_tasks=['']
 task_list = f"Your current tasks are :{ user_tasks}"
-print(task_list)
 
-
-add_or_del =input( "would you like to add or remove  a task? :" )
-while add_or_del == "add":
-    user_tasks.append(input('add a task :'))
-    task_list = f"Your current tasks are :{ user_tasks[1:200]}"
+#add_or_del =input( "would you like to add or remove  a task? :" )
+while True: 
     print(task_list)
-    if add_or_del== 'remove':
-        continue
-        user_tasks.remove(input('remove a task :'))
+    print("____________________________________________________")
+    add_or_del =input( "would you like to add or remove  a task? :" )
+    while add_or_del == "add":
+        new_task= input('add a task :')
+        user_tasks.append(new_task +'\n')
         task_list = f"Your current tasks are :{ user_tasks[1:200]}"
         print(task_list)
+        print("____________________________________________________")
+        add_or_del =input( "would you like to add or remove  a task? :" ) 
+    while add_or_del == "remove":
+        user_tasks.remove(input('remove a task :'))
+        task_list = f"Your current tasks are :{ user_tasks[1:200]}                                                                                                                      ________________________________________________________"
+        print(task_list)
+        print("____________________________________________________")
+        add_or_del =input( "would you like to add or remove  a task? :" )
+        print("____________________________________________________")
         
-else:
-    print('sorry I dont understand')
+        
+else :
+with open("task_list.txt","w") as file:
+file.writelines(user_tasks)
