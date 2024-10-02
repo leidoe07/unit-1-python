157f77b74c85bae6e3871c26029e05574088a219
Leilani Dore <ldore@brooklynsteamcenter.org>
2024-10-02 08:44:14 -0400

    adding documents to finish

---
+++ 07_flipping_fors.py
@@ -0,0 +1,24 @@
+"""
+Exercise 1:
+Write a program to print each character of a given string using a for loop.
+"""
+
+
+"""
+Exercise 2:
+Write a program to calculate the sum of elements in a list using a for loop.
+"""
+
+"""
+Exercise 3:
+Write a program to print the length of each word in a sentence using a for loop and a list.
+"""
+
+"""
+Excercise 4:
+Write a program that creates a dictionary (atleast 4 key:value pairs) and then
+iterates through a dictionary and prints the result
+
+In a comment, answer the following, what do you notice about the output of your code?
+Is it what you expected?
+"""
---
+++ 07_rootin_ranges.py
@@ -0,0 +1,35 @@
+"""
+Exercise 1:
+Write a program to print numbers from 1 to 10 using a for loop.
+"""
+
+
+"""
+Exercise 2:
+Write a program to count by 10s from 900 to 1000
+"""
+
+"""
+Exercise 3:
+Write a program that counts form 1-100 by 9
+"""
+
+"""
+Exercise 4:
+Write a program to calculate the sum of all numbers from 1 to 10 using a for loop.
+"""
+
+"""
+Excercise 5:
+Uncomment the following code and run it. Then answer the following:
+- What is the ouput of the code?
+
+- Explain the iterative process that this code executes to get that output
+"""
+
+# rows = 5
+# 
+# for i in range(rows):
+#     for j in range(i + 1):
+#         print('*', end=' ')
+#     print()
