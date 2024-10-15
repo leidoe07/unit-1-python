dde2a2caf2844ba33cd3e5dec60fdcfbecf9844e
Leilani Dore <ldore@brooklynsteamcenter.org>
2024-10-15 10:26:17 -0400

    added missing files

---
+++ 12_functions.py
@@ -0,0 +1,46 @@
+"""
+Task 1: Greeting
+Write a function that takes a name as a 
+agrument and prints a greeting message like "Hello, [name]!".
+"""
+
+"""
+Task 2: Square of a Number
+Write a function that takes an integer as an argument and returns its square.
+"""
+
+
+"""
+Task 3: Even or Odd
+Write a function that takes a number as a argument that 
+returns `True` if the number is even, and `False` otherwise.
+"""
+
+
+"""
+Task 4: Area of a Rectangle
+Write a function that takes the length and width of a rectangle and returns its area.
+"""
+
+
+"""
+Task 5: Celsius to Fahrenheit
+Write a function that takes a temperature in Celsius and returns 
+the equivalent temperature in Fahrenheit using the correct formula
+"""
+
+
+"""
+Task 6: Average of Numbers
+Write a function that takes a list of numbers as an argument
+and returns the average (mean) of those numbers.
+"""
+
+"""
+Task 7: Total Calculator
+Create a function that has arguments for the price and quantity of something, 
+and returns the total.
+
+Your function should also optionally accept a 3rd argument for discount as a float, 
+and return the discounted if provided.
+"""
---
+++ 13_classical_classes.py
@@ -0,0 +1,27 @@
+
+"""
+Task 1: People Class
+Define a class called Person with attributes name and age.
+Then, write a method within the class to introduce the person with their name and age.
+
+Create a new object using this new class, and call the method you created.
+"""
+
+"""
+Task 2: Animals Speak
+Create a base class Animal with a empty method called speak. 
+
+Then create two subclasses, Dog and Cat, each with their own speak method. 
+
+Create objects using these subclasses and call the speak method.
+"""
+
+
+"""
+Task 3: Banking
+Create a class BankAccount with attributes balance and owner. 
+
+Include methods for depositing and withdrawing money, which should modify the balance attribute.
+
+Test these methods with instances of the class.
+"""
