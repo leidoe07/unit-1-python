26dffdb119f1dff444d06c2b96857c504df8fadf
Leilani Dore <ldore@brooklynsteamcenter.org>
2024-10-16 10:50:11 -0400

    mostly completed trippy_testing must finish notes

---
+++ 15_trippy_testing.py
@@ -0,0 +1,107 @@
+"""
+Exercise 1: Divide
+"""
+def divide(a, b):
+  """Divides two numbers, handling potential division by zero.
+
+  Args:
+    a: The numerator.
+    b: The denominator.
+
+  Returns:
+    The quotient, or None if b is zero.
+  """
+
+  if b == 0:
+    return None
+    assert divide(8, 0) == 0  #
+  else:
+    return a / b
+    assert divide(8, 4) == 2 #
+  
+"""
+Exercise 2: Factorial
+"""
+def factorial(n):
+  """Calculates the factorial of a non-negative integer.
+
+  Args:
+    n: A non-negative integer.
+
+  Returns:
+    The factorial of n.
+  """
+
+  if n == 0:
+    return 1
+    assert factorial(0) == 1# checks if the factorial of 0 is properly
+  else:
+    return n * factorial(n - 1)
+  assert factorial(8) == 40320#checks if the factorial of other numbers properly calculated
+"""
+Exercise 3: String Reverse
+"""
+def reverse_string(string):
+  """Reverses a given string.
+
+  Args:
+    string: A string to be reversed.
+
+  Returns:
+    The reversed string.
+  """
+
+  reversed_string = ""
+  for char in string:
+    reversed_string = char + reversed_string
+  return reversed_string
+  
+  assert reverse_string(string) == reversed_string# i think i did this wrong
+
+
+"""
+Exercise 4: Fibonacci
+"""
+def fibonacci(n):
+  """Generates the nth Fibonacci number.
+
+  Args:
+    n: The index of the Fibonacci number to calculate.
+
+  Returns:
+    The nth Fibonacci number.
+  """
+
+  if n <= 0:
+    return 0
+  elif n == 1:
+    return 1
+  else:
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+  assert fibonacci(1) ==1    
+  assert fibonacci(9) ==4181
+#
+
+
+"""
+Exercise 5: Email Validation
+"""
+
+import re
+
+def is_valid_email(email):
+  """Determines whether email is valid or not
+
+  Args:
+    email: The email to validate
+
+  Returns:
+    Boolean value if email is valid
+  """
+  email_regex = r"^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+"
+  return re.match(email_regex, email) is not None
+
+  assert is_valid_email(leilanidore07@gmail.com)==1   #
+  
+  assert is_valid_email(thissite.google.com)!=1 #
